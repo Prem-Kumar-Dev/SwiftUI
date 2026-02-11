88b16f88845df28369a85e8f5cfb827138837966
Prem_Kumar <krprem514@gmail.com>
2026-02-11 11:23:54 +0530

    11th feb

---
+++ StudentPerformancePredictor/StudentPerformancePredictor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudentPerformancePredictor
+//
+//  Created by Prem Kumar on 11/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudentPerformancePredictor/StudentPerformancePredictor/StudentPerformancePredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentPerformancePredictorApp.swift
+//  StudentPerformancePredictor
+//
+//  Created by Prem Kumar on 11/02/26.
+//
+
+import SwiftUI
+
+@main
+struct StudentPerformancePredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
